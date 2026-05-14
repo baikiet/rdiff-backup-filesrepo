70eda053a9b121126fd8b298cd3ded4832d4a988
baikiet <126314581+baikiet@users.noreply.github.com>
2026-05-14 19:09:48 +0200

    Make rdiff-backup_testfiles.fix.sh idempotent.
    When running `rdiff-backup_testfiles.fix.sh` multiple times, `chown` and/or `chgrp` will eventually fail as they will be called without any argument. This change ensures that if `find` does not find any file, `xargs` does not run `chown`/`chgrp`.

--- rdiff-backup_testfiles.fix.sh
+++ rdiff-backup_testfiles.fix.sh
@@ -1,4 +1,4 @@
-find rdiff-backup_testfiles -user 1000 \! -type l -print0 | xargs -0 chown -v ${1:-$SUDO_UID}
-find rdiff-backup_testfiles -group 1000 \! -type l -print0 | xargs -0 chgrp -v ${2:-$SUDO_GID}
-find rdiff-backup_testfiles -user 1000 -type l -print0 | xargs -0 chown -vh ${1:-$SUDO_UID}
-find rdiff-backup_testfiles -group 1000 -type l -print0 | xargs -0 chgrp -vh ${2:-$SUDO_GID}
+find rdiff-backup_testfiles -user 1000 \! -type l -print0 | xargs -r -0 chown -v ${1:-$SUDO_UID}
+find rdiff-backup_testfiles -group 1000 \! -type l -print0 | xargs -r -0 chgrp -v ${2:-$SUDO_GID}
+find rdiff-backup_testfiles -user 1000 -type l -print0 | xargs -r -0 chown -vh ${1:-$SUDO_UID}
+find rdiff-backup_testfiles -group 1000 -type l -print0 | xargs -r -0 chgrp -vh ${2:-$SUDO_GID}
